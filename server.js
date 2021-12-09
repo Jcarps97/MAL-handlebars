c94b22d1d79d3f9055740cf6dc6cb67e7395390c
Jack Carpenter <jackcarpenter1@live.com>
2021-12-08 18:38:08 -0600

    Server starts up, working on routes, next

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 
 const sequelize = require("./config/connection.js");
