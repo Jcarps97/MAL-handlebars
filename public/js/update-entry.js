e86861fbc0059d5fcfe93b7e72d9deccd41bded7
Jack Carpenter <jackcarpenter1@live.com>
2021-12-17 12:06:50 -0600

    Added update functionality, will work on status data point, next

---
+++ public/js/update-entry.js
@@ -0,0 +1,30 @@
+const updateEntryHandler = async (event) => {
+    event.preventDefault();
+
+    const entry_id = document.querySelector('#edit-entry-id').value;
+    const episodes_seen = document.querySelector('#edited-episodes').value.trim();
+    const rating = document.querySelector('#edited-rating').value.trim();
+    const title = document.querySelector('#edited-title').value.trim();
+
+
+    if (episodes_seen && rating && title) {
+        const response = await fetch(`/api/entry/${entry_id}`, {
+            method: 'PUT',
+            body: JSON.stringify({title, episodes_seen, rating}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            window.alert('Entry info updated!');
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to update entry');
+        }
+    }
+}
+
+document
+    .querySelector('#edit-entry-button')
+    .addEventListener('click', updateEntryHandler);
